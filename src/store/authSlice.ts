fececec58cff46212fa2acb5fa00772baacaedbf
54ntu <chaudharysantaram8@gmail.com>
2026-02-24 09:32:35 +0000

    registerUser api is called in authSlice.ts file with the help of thunk only we can do that

--- src/store/authSlice.ts
+++ src/store/authSlice.ts
@@ -1,5 +1,7 @@
 import { createSlice, type PayloadAction } from "@reduxjs/toolkit";
 import { Status } from "../globals/types/type";
+import axios from "axios";
+import type { AppDispatch } from "./store";
 
 
 interface IUser {
@@ -43,3 +45,25 @@ const authSlice = createSlice({
 
 export const { setStatus, setUser } = authSlice.actions
 export default authSlice.reducer
+
+
+export function registerUer(data: IUser) {
+    return async function registerUserThunk(dispatch: AppDispatch) {
+        try {
+            const response = await axios.post("http://localhost:4000/api/v1/users/register", data)
+            if (response.status === 200) {
+                dispatch(setStatus(Status.SUCCESS))
+            } else {
+                dispatch(setStatus(Status.ERROR))
+            }
+        } catch (error) {
+            console.log(error)
+            dispatch(setStatus(Status.ERROR
+
+            ))
+
+
+        }
+
+    }
+}
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,10 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
+import authSlice from "./authSlice.ts"
 
 
 
 const store = configureStore({
     reducer: {
-        
+        auth: authSlice
     }
 })
 
